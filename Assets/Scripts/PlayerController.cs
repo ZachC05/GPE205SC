c19817f7d8a1fa67cf077f9fee53a092c5d0d781
ZachC05 <ZCHRISTMAN86465@uat.edu>
2025-01-28 15:31:22 -0700

    2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class PlayerController : Controller
 {
+    //Keys are use to get input from players
+    [Header("Keys for Moving")]
+    public KeyCode moveForwardKey;
+    public KeyCode moveBackwardKey;
 
+    [Header("Keys for Rotations")]
+    public KeyCode rotateRightKey;
+    public KeyCode rotateLeftKey;
 
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        GetInputs();
+        base.Update();
+    }
+
+    public override void GetInputs()
+    {
+        //gets inputs from player and communicates it to the pawn scripts
+        if (Input.GetKeyDown(moveForwardKey))
+        {
+            pawn.MoveForward();
+        }
+        if (Input.GetKeyDown(moveBackwardKey))
+        {
+            pawn.MoveBackward();
+        }
+        if (Input.GetKeyDown(rotateRightKey))
+        {
+            pawn.RotateRight();
+        }
+        if (Input.GetKeyDown(rotateLeftKey))
+        {
+            pawn.RotateLeft();
+        }
     }
 }
--- Assets/Scripts/Shooters/TankShooter.cs
+++ Assets/Scripts/Shooters/TankShooter.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Controller : MonoBehaviour
+public class TankShooter : Shooter
 {
-    //variable to hold the pawn
-    public Pawn pawn;
     // Start is called before the first frame update
-    public virtual void Start()
+    void Start()
     {
         
     }
 
     // Update is called once per frame
-    public virtual void Update()
+    void Update()
     {
         
     }
-    public abstract void GetInputs();
 }
